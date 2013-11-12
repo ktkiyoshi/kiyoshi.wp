2dc41c1db727433193953c08bb7c6320e8db1f77
kodamat <kt.kiyoshi@gmail.com>
2013-11-13 00:05:20 +0900

    Add recent 3 photos on index

--- index.php
+++ index.php
@@ -11,6 +11,10 @@ jQuery(document).ready(function($) {
       $('#box_entry').slideToggle(500)
       return false;
     });
+    $("#old_photos").click(function(){
+      $('#box_photos').slideToggle(500)
+      return false;
+    });
     $("#old_history").click(function(){
       $('#box_history').slideToggle(500)
       return false;
@@ -150,26 +154,26 @@ jQuery(document).ready(function($) {
     <!-- tabs menu end -->
 
     <div class="l_frame">
-      <h1>Recent Gallery<a href="" id="old_history"><img src="<?php bloginfo('template_directory'); ?>/img/zoom_icon&16.png" class="more" /></a></h1>
+      <h1>Recent 3 Photos</h1>
     </div>
     <?php
-     for($i=0; $i < count($media); $i++) {
+     //for($i=0; $i < count($media); $i++) {
+     for($i=0; $i < 3; $i++) {
       $caption = $media[$i][caption];
       $images = $media[$i][images];
     ?>
     <div class="photos">
-        <p class="exp_1">
-          <?php echo date('Y/m/d', $caption[created_time]); ?> 
-        </p>
-        <p><a href="<?php echo $media[$i][link]; ?>" title="<?php echo $caption[text]; ?>">
-          <img src="<?php echo $images[thumbnail][url]; ?>" class="img_yoko"/></a></p>
-          <span class="exp_2"><a href="<?php echo $media[$i][link]; ?>"><?php echo $caption[text]; ?></a></span>
-        </div>
-        <?php if (($i%3) == 2 || ($i+1) == count($media)) { ?>
-        <div class="reset"><br /></div>
-        <?php } ?>
-  <?php } ?>
+      <p class="exp_1">
+        <?php echo date('Y/m/d', $caption[created_time]); ?> 
+      </p>
+      <p><a href="<?php echo $media[$i][link]; ?>" title="<?php echo $caption[text]; ?>">
+      <img src="<?php echo $images[thumbnail][url]; ?>" class="img_yoko"/></a></p>
+      <span class="exp_2"><a href="<?php echo $media[$i][link]; ?>"><?php echo $caption[text]; ?></a></span>
     </div>
+    <?php if (($i%3) == 2 || ($i+1) == count($media)) { ?>
+    <div class="reset"><br /></div>
+    <?php } ?>
+    <?php } ?>
 
     <div class="l_frame">
       <h1>Update History<a href="" id="old_history"><img src="<?php bloginfo('template_directory'); ?>/img/zoom_icon&16.png" class="more" /></a></h1>
