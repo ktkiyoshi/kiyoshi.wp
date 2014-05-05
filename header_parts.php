464bc0d1e2d8d2eadf2ba1d4bb1391dfc687cb9f
Kiyoshi <kt.kiyoshi@gmail.com>
2014-05-05 09:31:05 +0900

    [TMP] Design Renewal Modify entry's mergin

--- header_parts.php
+++ header_parts.php
@@ -2,7 +2,7 @@
 <h1><a href="/wp">超日記</a></h1>
 <nav>
   <ul>
-    <li><a href="">ABOUT</a></li>
+    <li><a href="/wp/about">ABOUT</a></li>
     <li><a href="/wp/gallery">GALLERY</a></li>
   </ul>
 </nav>
