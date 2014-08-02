e97fc954eb6b46f6dbac29dcd4924b7b7707f661
Kiyoshi <kt.kiyoshi@gmail.com>
2014-08-03 01:48:08 +0900

    Remove Google Analytics from footer.php because of starting to use plugin

--- footer.php
+++ footer.php
@@ -76,6 +76,7 @@ jQuery(document).ready(function($) {
 </script>
 
 <!-- Google Analytics -->
+<!--
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -84,6 +85,7 @@ jQuery(document).ready(function($) {
   ga('create', 'UA-31926749-1', 'kt-kiyoshi.com');
   ga('send', 'pageview');
 </script>
+-->
 
 <!-- Google+ -->
 <script type="text/javascript">
