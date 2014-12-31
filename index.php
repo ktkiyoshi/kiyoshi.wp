ebe733c8d604c83cd17f70cd97229563788e8c62
Takahiro Kodama <kt.kiyoshi@gmail.com>
2014-12-31 10:58:55 +0900

    Add BOOKS category on the top of index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <ul class="panels t_center">
           <li class="panel_title"><a href="#panel1">最新記事</a></li>
           <li class="panel_title"><a href="#panel2">技術記事</a></li>
+          <li class="panel_title"><a href="#panel3">読書記事</a></li>
         </ul>
 
         <div id="panel1" class="panel">
@@ -60,6 +61,32 @@
         <?php wp_reset_postdata(); ?>
         </div>
       </section>
+
+      <section>
+        <div id="panel3" class="panel">
+        <?php global $post; $mypost = get_posts( array( 'numberposts' => 10, 'category' => 26 ));?>
+        <?php foreach( $mypost as $post ) : setup_postdata($post); ?>
+          <article class="index mb20">
+            <header>
+              <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+              <ul class="entry_meta">
+                <li><time datetime="<?php the_time('Y-m-d (D) G:i') ?>" pubdate><?php the_time('Y-m-d (D) G:i') ?></time></li>
+                <li>| <?php the_category(' | ') ?></li>
+                <li>| <?php edit_post_link('Edit', '<span class="admin">', '</span>'); ?></li>
+              </ul>
+            </header>
+            <div class="entry_info">
+              <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><img src="<?php echo catch_that_image(); ?>" class="thumbnail_B f_left"/></a>
+              <p class="description_B"><?php echo mb_substr(get_the_excerpt(),0,100);?>...</p>
+              <p class="entry_more ml215"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">&raquo;続きを読む</a></p>
+            </div>
+          </article>
+          <div class="reset"></div>
+        <?php endforeach; ?>
+        <?php wp_reset_postdata(); ?>
+        </div>
+      </section>
+
       </nav>
     </div><!-- /#content -->
 <?php get_sidebar(); ?>
