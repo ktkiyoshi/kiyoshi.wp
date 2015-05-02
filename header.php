1c2cc5685cd66abde3d6005c1db875be3db59a63
Takahiro Kodama <kt.kiyoshi@gmail.com>
2015-05-03 07:51:40 +0900

    Delete wp_enqueue_script( 'jquery' ) from header

--- header.php
+++ header.php
@@ -6,6 +6,5 @@
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <meta property="fb:app_id" content="112219258880440" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
-<?php wp_enqueue_script( 'jquery' ); ?>
 <?php wp_head(); ?>
 </head>
