fe38da2533f3251b89b8fc41f9620e2cf3979d1d
Takahiro Kodama <kt.kiyoshi@gmail.com>
2015-09-14 00:17:25 +0900

    Add new style for IT entries

--- header.php
+++ header.php
@@ -6,5 +6,6 @@
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <meta property="fb:app_id" content="112219258880440" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+<script type="text/javascript" charset="utf-8" src="//use.edgefonts.net/source-code-pro.js"></script>
 <?php wp_head(); ?>
 </head>
