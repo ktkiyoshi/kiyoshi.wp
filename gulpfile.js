401f1b488d592dfacc3aa905b8146a3d1cb1b7ac
Takahiro Kodama <kt.kiyoshi@gmail.com>
2017-04-04 23:05:50 +0900

    Add ‘gulp’ and minify CSSs

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var rename       = require('gulp-rename');
+
+//JS圧縮
+/*
+gulp.task('minify-js', function() {
+    return gulp.src("js/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'));
+        //.pipe(gulp.dest('js')); 上書きする場合
+});
+*/
+
+//CSS圧縮
+gulp.task('minify-css', function() {
+    return gulp.src([
+        'css/*.css',
+        '!css/font-awesome.css',
+        '!css/font-awesome.min.css',
+        '!css/amazonjs.css'
+        ])
+        .pipe(cleanCSS())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('css/dist/'));
+        //.pipe(gulp.dest('css')); 上書きする場合
+});
