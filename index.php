57db871769978365456fe0295452b011cb1a2606
Takahiro Kodama <kt.kiyoshi@gmail.com>
2017-04-29 17:33:38 +0900

    Add pagenation to the index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-<?php query_posts("cat=-80"); ?>
 <body>
 <?php require("header_parts.php"); ?>
 <div id="wrapper">
@@ -23,13 +22,31 @@
         </ul>
 -->
         <div id="panel1" class="panel">
-          <?php
+        <?php
             $query_array = $wp_query->query_vars;
             $query_array['posts_per_page'] = 10;
             query_posts($query_array);
-            while (have_posts()) : the_post(); $counter++;
-            if ($counter == 1) {
-          ?>
+            $paginate_base = get_pagenum_link(1);
+            if (strpos($paginate_base, '?') || ! $wp_rewrite->using_permalinks()) {
+                $paginate_format = '';
+                $paginate_base = add_query_arg('paged', '%#%');
+            } else {
+                $paginate_format = (substr($paginate_base, -1 ,1) == '/' ? '' : '/') .user_trailingslashit('page/%#%/', 'paged');
+                $paginate_base .= '%_%';
+            }
+            $pagination = array('base' => $paginate_base,
+                                'format' => $paginate_format,
+                                'total' => $wp_query->max_num_pages,
+                                'mid_size' => 5,
+                                'current' => ($paged ? $paged : 1)
+                                );
+            echo '<div class="page-navi">'."\n";
+            echo paginate_links($pagination);
+            echo '</div>'."\n";
+
+            while (have_posts()) : the_post(); $cnt++;
+            if ($cnt == 1) {
+        ?>
             <article>
               <header>
                 <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
@@ -45,7 +62,7 @@
                 <p class="entry_more"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">&raquo;続きを読む</a></p>
               </div>
             </article>
-          <?php } else if ($counter < 10) { ?>
+          <?php } else if ($cnt != 1) { ?>
             <article class="index matchHeight">
               <header>
                 <ul class="entry_meta">
@@ -61,11 +78,14 @@
               </div>
             </article>
           <?php
-            } else if ($counter == 10) { break; }
+            }
             endwhile;
+            echo '<div class="page-navi">'."\n";
+            echo paginate_links($pagination);
+            echo '</div>'."\n";
+            wp_reset_query();
           ?>
         </div>
-        <?php wp_reset_query(); ?>
 <!--
       <section>
         <div id="panel2" class="panel">
