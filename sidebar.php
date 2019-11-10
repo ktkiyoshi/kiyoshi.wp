03643e2946c49a63626d30e44d5f4a1a5c2c0ce7
Takahiro Kodama <kt.kiyoshi@gmail.com>
2019-11-10 10:20:32 +0900

    [develop] Change CSS and sorting of sidebar

--- sidebar.php
+++ sidebar.php
@@ -5,52 +5,8 @@
     <script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="0c90124c.ed5776d5.0c90124d.28929496";rakuten_items="ctsmatch";rakuten_genreId=0;rakuten_size="200x350";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="off";rakuten_auto_mode="on";rakuten_genre_title="off";rakuten_recommend="on";</script><script type="text/javascript" src="https://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script>
     </div>
 -->
-    <div class="r_frame">
-      <p class="title">人気記事ランキング</p>
-    </div>
-    <section>
-      <ul>
-      <?php
-        // $populars = popularRanking(); // Old data
-        $populars = viewingRanking();
-        foreach($populars as $post) {
-        ?>
-        <li>
-          <a href="<?php echo $post['url']; ?>" title="<?php echo $post['post_title']; ?>">
-            <img src="<?php echo $post['image']; ?>" class="thumbnail_C f_left"/>
-            <time datetime="<?php echo $post['date']; ?>" pubdate><?php echo $post['date']; ?></time>
-            <p class="postTitle">
-            <?php if(mb_strlen($post['post_title'])>80) {
-                    $title= mb_substr($post['post_title'],0,80);
-                    echo $title. ･･･ ;
-                  } else {
-                    echo $post['post_title'];
-                  }?>
-            </p>
-            <p class="t_right tx_under fo_italy"><?php echo number_format($post['count']);?> views</p>
-          </a>
-        </li>
-        <?php } ?>
-      </ul>
-    </section>
 
-    <div class="r_frame">
-      <p class="title">写真日記</p>
-    </div>
-    <section class="t_center">
-      <ul>
-      <?php global $post; $mypost = get_posts( array( 'numberposts' => 2, 'post_type' => 'photo' ));?>
-      <?php foreach( $mypost as $post ) : setup_postdata($post); ?>
-        <li class="photoDiary">
-            <a href="<?php the_permalink(); ?>"><img src="<?php echo catch_that_image("thumbnail"); ?>" class="thumbnail_F" /></a>
-            <time datetime="<?php the_time('Y/m/d/D') ?>" pubdate><?php the_time('Y/m/d/D') ?></time>
-        </li>
-      <?php endforeach; ?>
-      </ul>
-      <p class="link"><a href="/wp/photo">写真日記一覧を見る</p>
-    </section>
-
-    <div class="r_frame">
+  <div class="r_frame">
       <p class="title">最新記事</p>
     </div>
     <section id="fixed_point">
@@ -77,6 +33,51 @@
           </a>
         </li>
         <?php } endforeach; ?>
+      </ul>
+    </section>
+
+    <div class="r_frame">
+      <p class="title">写真日記</p>
+    </div>
+    <section class="t_center">
+      <ul>
+      <?php global $post; $mypost = get_posts( array( 'numberposts' => 2, 'post_type' => 'photo' ));?>
+      <?php foreach( $mypost as $post ) : setup_postdata($post); ?>
+        <li class="photoDiary">
+            <a href="<?php the_permalink(); ?>"><img src="<?php echo catch_that_image("thumbnail"); ?>" class="thumbnail_F" /></a>
+            <time datetime="<?php the_time('Y/m/d/D') ?>" pubdate><?php the_time('Y/m/d/D') ?></time>
+        </li>
+      <?php endforeach; ?>
+      </ul>
+      <p class="link"><a href="/wp/photo">写真日記一覧を見る</p>
+    </section>
+
+    <div class="r_frame">
+      <p class="title">人気記事ランキング</p>
+    </div>
+    <section>
+      <ul>
+      <?php
+        // $populars = popularRanking(); // Old data
+        $populars = viewingRanking();
+        foreach($populars as $post) {
+        ?>
+        <li>
+          <a href="<?php echo $post['url']; ?>" title="<?php echo $post['post_title']; ?>">
+            <img src="<?php echo $post['image']; ?>" class="thumbnail_C f_left"/>
+            <time datetime="<?php echo $post['date']; ?>" pubdate><?php echo $post['date']; ?></time>
+            <p class="postTitle">
+            <?php if(mb_strlen($post['post_title'])>80) {
+                    $title= mb_substr($post['post_title'],0,80);
+                    echo $title. ･･･ ;
+                  } else {
+                    echo $post['post_title'];
+                  }?>
+            </p>
+            <p class="t_right tx_under fo_italy"><?php echo number_format($post['count']);?> views</p>
+          </a>
+        </li>
+        <?php } ?>
       </ul>
     </section>
 
