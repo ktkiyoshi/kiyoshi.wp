4a4738d43eb5fb9fc2e39fd9699e6e0cbaccecd4
Takahiro Kodama <kt.kiyoshi@gmail.com>
2024-01-24 22:53:27 +0900

    Delete gtag due to migration to Site Kit as WP plugin

--- header.php
+++ header.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title><?php wp_title ( '|', true,'right' ); ?><?php bloginfo('name'); ?></title>
-<meta charset="<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-<meta property="fb:app_id" content="112219258880440" />
-<?php wp_head(); ?>
-</head>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-1S514H4VG9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-1S514H4VG9');
+<head>
+  <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta property="fb:app_id" content="112219258880440" />
+  <?php wp_head(); ?>
+</head>
 </script>
