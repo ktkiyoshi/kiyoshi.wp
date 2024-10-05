ed9841e54e9ca7e9ee38f0f5c236971a20f57736
Takahiro Kodama <kt.kiyoshi@gmail.com>
2024-10-05 22:51:28 +0900

    Hide instagram link on the header

--- parts/header_link.php
+++ parts/header_link.php
@@ -10,7 +10,7 @@
             <ul>
                 <li><span class="">超日記について</span><a href="<?php echo home_url('/about'); ?>">ABOUT</a></li>
                 <li><span class="">私のnote</span><a href="<?php echo home_url('/note'); ?>">NOTE</a></li>
-                <li><span class="">私のインスタ</span><a href="<?php echo home_url('/gallery'); ?>">INSTA</a></li>
+                <!-- <li><span class="">私のインスタ</span><a href="<?php echo home_url('/gallery'); ?>">INSTA</a></li> -->
                 <li><span class="">技術エントリー</span><a href="<?php echo home_url('/tech'); ?>">TECH-BLOG</a></li>
             </ul>
         </nav>
