ab6443aab217adab2a33903e81663db6574b3ebc
Takahiro Kodama <kt.kiyoshi@gmail.com>
2024-10-06 00:10:17 +0900

    Delete facebook app_id

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
   <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <meta property="fb:app_id" content="112219258880440" />
   <?php wp_head(); ?>
 </head>
 </script>
